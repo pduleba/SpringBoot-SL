ce9f1d3be34f7ee82710103e6199785311d1eaea
pduleba <pduleba@pgs-soft.com>
2018-02-07 15:30:30 +0100

    Error handling

--- spring-boot-quickstart/src/main/java/com/pduleba/spring/boot/handler/ErrorHandlerConfig.java
+++ spring-boot-quickstart/src/main/java/com/pduleba/spring/boot/handler/ErrorHandlerConfig.java
@@ -1,28 +1,42 @@
 package com.pduleba.spring.boot.handler;
 
+import java.util.Map;
+
+import org.springframework.boot.autoconfigure.web.*;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.*;
-import org.springframework.web.filter.CommonsRequestLoggingFilter;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import com.pduleba.spring.boot.App;
 
 @Configuration
 public class ErrorHandlerConfig {
 
-	// @Bean
-	// public ServletRegistrationBean dispatcherRegistration() {
-	// return new ServletRegistrationBean(dispatcherServlet());
-	// }
-	//
-	// @Bean(name =
-	// DispatcherServletAutoConfiguration.DEFAULT_DISPATCHER_SERVLET_BEAN_NAME)
-	// public DispatcherServlet dispatcherServlet() {
-	// return new LoggableDispatcherServlet();
-	// }
+	private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(App.class);
 
+	// RESTFULL ERROR HANDLING :: I Approach
 	@Bean
-	public CommonsRequestLoggingFilter requestLoggingFilter() {
-		CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
-		loggingFilter.setIncludeClientInfo(true);
-		loggingFilter.setIncludeQueryString(true);
-		loggingFilter.setIncludePayload(true);
-		return loggingFilter;
+	public ServletRegistrationBean dispatcherRegistration() {
+		return new ServletRegistrationBean(dispatcherServlet());
+	}
+
+	@Bean(name = DispatcherServletAutoConfiguration.DEFAULT_DISPATCHER_SERVLET_BEAN_NAME)
+	public DispatcherServlet dispatcherServlet() {
+		return new LoggableDispatcherServlet();
+	}
+
+	// RESTFULL ERROR HANDLING :: II Approach
+	@Bean
+	public ErrorAttributes errorAttributes() {
+		return new DefaultErrorAttributes() {
+			@Override
+			public Map<String, Object> getErrorAttributes(RequestAttributes requestAttributes,
+					boolean includeStackTrace) {
+				Map<String, Object> errorAttributes = super.getErrorAttributes(requestAttributes, includeStackTrace);
+				log.error("ErrorHandler (I Approach) :: " + errorAttributes.toString());
+				return errorAttributes;
+			}
+		};
 	}
 }
--- spring-boot-quickstart/src/main/java/com/pduleba/spring/boot/handler/LoggableDispatcherServlet.java
+++ spring-boot-quickstart/src/main/java/com/pduleba/spring/boot/handler/LoggableDispatcherServlet.java
@@ -1,6 +1,6 @@
 package com.pduleba.spring.boot.handler;
 
-import java.io.IOException;
+import java.io.*;
 
 import javax.servlet.http.*;
 
@@ -9,7 +9,7 @@ import org.springframework.web.util.*;
 
 public class LoggableDispatcherServlet extends DispatcherServlet {
 
-	private static final long serialVersionUID = 2923805735583620142L;
+	private static final long serialVersionUID = 843988381166635080L;
 
 	@Override
 	protected void doDispatch(HttpServletRequest request, HttpServletResponse response) throws Exception {
@@ -19,14 +19,38 @@ public class LoggableDispatcherServlet extends DispatcherServlet {
 		if (!(response instanceof ContentCachingResponseWrapper)) {
 			response = new ContentCachingResponseWrapper(response);
 		}
-		HandlerExecutionChain handler = getHandler(request);
 
 		try {
 			super.doDispatch(request, response);
 		} finally {
-			// log(request, response, handler);
+			HandlerExecutionChain handler = getHandler(request);
+			log(request, response, handler);
 			updateResponse(response);
 		}
+	}
+
+	private void log(HttpServletRequest requestToCache, HttpServletResponse responseToCache,
+			HandlerExecutionChain handler) {
+		String payload = getResponsePayload(responseToCache);
+		logger.error("ErrorHandler (II Approach) :: " + payload);
+	}
+
+	private String getResponsePayload(HttpServletResponse response) {
+		ContentCachingResponseWrapper wrapper = WebUtils.getNativeResponse(response,
+				ContentCachingResponseWrapper.class);
+		if (wrapper != null) {
+
+			byte[] buf = wrapper.getContentAsByteArray();
+			if (buf.length > 0) {
+				int length = Math.min(buf.length, 5120);
+				try {
+					return new String(buf, 0, length, wrapper.getCharacterEncoding());
+				} catch (UnsupportedEncodingException ex) {
+					// NOOP
+				}
+			}
+		}
+		return "[unknown]";
 	}
 
 	private void updateResponse(HttpServletResponse response) throws IOException {
