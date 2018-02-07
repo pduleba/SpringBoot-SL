3bb9b580d15cda6d55eb22aa7ed00d8b4b20cb1a
pduleba <pduleba@pgs-soft.com>
2018-02-07 16:03:41 +0100

    Application properties

--- spring-boot-quickstart/src/main/java/com/pduleba/spring/boot/App.java
+++ spring-boot-quickstart/src/main/java/com/pduleba/spring/boot/App.java
@@ -6,7 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication // includes @EnableAutoConfiguration
 public class App {
 
+	private static final String SPRING_PROFILES_ACTIVE = "spring.profiles.active";
+
 	public static void main(String[] args) {
+		// System.setProperty(SPRING_PROFILES_ACTIVE, "test");
+		System.setProperty(SPRING_PROFILES_ACTIVE, "dev");
+		// System.setProperty(SPRING_PROFILES_ACTIVE, "prod");
 		SpringApplication.run(App.class, args);
 	}
 
